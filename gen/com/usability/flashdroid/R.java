07788759c480e03348ae389c7e1f7f5a4a920fa8
EJ <ejd7856@rit.edu>
2012-10-11 20:19:07 -0400

    Adjusted main screen layout and added string references for button text

--- gen/com/usability/flashdroid/R.java
+++ gen/com/usability/flashdroid/R.java
@@ -15,12 +15,11 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int OuterGrid=0x7f070001;
-        public static final int button1=0x7f070000;
-        public static final int button2=0x7f070002;
-        public static final int button3=0x7f070003;
-        public static final int button4=0x7f070004;
-        public static final int menu_settings=0x7f070005;
+        public static final int manageButton=0x7f070002;
+        public static final int menu_settings=0x7f070004;
+        public static final int settingsButton=0x7f070003;
+        public static final int statisticsButton=0x7f070001;
+        public static final int studyButton=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -31,7 +30,11 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int manage=0x7f040005;
         public static final int menu_settings=0x7f040002;
+        public static final int settings=0x7f040007;
+        public static final int statistics=0x7f040006;
+        public static final int study=0x7f040004;
         public static final int title_activity_main=0x7f040003;
     }
     public static final class style {
