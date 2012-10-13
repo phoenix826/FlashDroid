b45929260bc404aca7f0beff83ddb2a8be5fd0a7
mattaddy <mxa5942@rit.edu>
2012-10-13 15:18:20 -0400

    Refactor button onclicks to different methods and add Toast notification

--- src/com/usability/flashdroid/AddDeckActivity.java
+++ src/com/usability/flashdroid/AddDeckActivity.java
@@ -4,12 +4,13 @@ import com.usability.flashdroid.data_source.DeckDataSource;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class AddDeckActivity extends Activity {
@@ -21,25 +22,6 @@ public class AddDeckActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_deck);
         // getActionBar().setDisplayHomeAsUpEnabled(true);
-        
-        deckSource = new DeckDataSource(this);
-        
-        Button saveButton = (Button) findViewById(R.id.saveDeck);
-        
-        saveButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-		        deckSource.open();
-		        
-				EditText deckNameText = (EditText) findViewById(R.id.deckNameText);
-				String deckName = deckNameText.getText().toString();
-				deckSource.createDeck(deckName);
-				
-				deckSource.close();
-				
-				Intent newIntent = new Intent(AddDeckActivity.this, ManagerActivity.class);
-				AddDeckActivity.this.startActivity(newIntent);
-			}
-		});
     }
 
     @Override
@@ -56,6 +38,27 @@ public class AddDeckActivity extends Activity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    public void saveDeck(View view) {
+        deckSource = new DeckDataSource(this);
+        deckSource.open();
+        
+		EditText deckNameText = (EditText) findViewById(R.id.deckNameText);
+		String deckName = deckNameText.getText().toString();
+		deckSource.createDeck(deckName);
+		
+		deckSource.close();
+		
+		Context context = getApplicationContext();
+		CharSequence text = "Successfully created deck";
+		int duration = Toast.LENGTH_LONG;
+
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
+		
+		Intent newIntent = new Intent(AddDeckActivity.this, ManagerActivity.class);
+		AddDeckActivity.this.startActivity(newIntent);
     }
 
 }
--- src/com/usability/flashdroid/ManagerActivity.java
+++ src/com/usability/flashdroid/ManagerActivity.java
@@ -5,15 +5,16 @@ import java.util.ArrayList;
 import com.usability.flashdroid.data_source.DeckDataSource;
 import com.usability.flashdroid.model.Deck;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
-public class ManagerActivity extends ListActivity {
+public class ManagerActivity extends Activity {
 	
 	private DeckDataSource deckSource;
 
@@ -25,24 +26,21 @@ public class ManagerActivity extends ListActivity {
         deckSource = new DeckDataSource(this);
         deckSource.open();
         
+        ListView deckList = (ListView) findViewById(R.id.deckList);
         ArrayList<Deck> decks = deckSource.getAllDecks();
         
-        setListAdapter(new ArrayAdapter<Deck>(this,
+        deckList.setAdapter(new ArrayAdapter<Deck>(this,
         	android.R.layout.simple_list_item_1, decks));
-        
-        Button addDeck = (Button) findViewById(R.id.addDeck);
-        
-        addDeck.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Intent newIntent = new Intent(ManagerActivity.this, AddDeckActivity.class);
-				ManagerActivity.this.startActivity(newIntent);
-			}
-		});
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_manager, menu);
         return true;
+    }
+    
+    public void addDeck(View view) {
+		Intent newIntent = new Intent(ManagerActivity.this, AddDeckActivity.class);
+		ManagerActivity.this.startActivity(newIntent);
     }
 }
