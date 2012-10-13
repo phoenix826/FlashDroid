a11a7920913ccf550702d15f6049e1cad7c119c6
mattaddy <mxa5942@rit.edu>
2012-10-13 11:29:45 -0400

    Use a ListActivity for the Manage Activity

--- src/com/usability/flashdroid/ManagerActivity.java
+++ src/com/usability/flashdroid/ManagerActivity.java
@@ -1,15 +1,31 @@
 package com.usability.flashdroid;
 
-import android.app.Activity;
+import java.util.ArrayList;
+
+import com.usability.flashdroid.data_source.DeckDataSource;
+import com.usability.flashdroid.model.Deck;
+
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
-public class ManagerActivity extends Activity {
+public class ManagerActivity extends ListActivity {
+	
+	private DeckDataSource deckSource;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
+        
+        deckSource = new DeckDataSource(this);
+        deckSource.open();
+        
+        ArrayList<Deck> decks = deckSource.getAllDecks();
+        
+        setListAdapter(new ArrayAdapter<Deck>(this,
+        	android.R.layout.simple_list_item_1, decks));
     }
 
     @Override
