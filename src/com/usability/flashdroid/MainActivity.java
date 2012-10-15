e1e66b445ea82072a39d37e7eb0e2919be467b79
William Wenzel <wrw3355@rit.edu>
2012-10-14 23:12:38 -0400

    Added the manage button functionality to the label as well.

--- src/com/usability/flashdroid/MainActivity.java
+++ src/com/usability/flashdroid/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -14,14 +15,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        ImageButton manageButton = (ImageButton) findViewById(R.id.manageButton);
-        
-        manageButton.setOnClickListener(new View.OnClickListener() {
+        final View.OnClickListener manageButtonListener = new View.OnClickListener() {
 			public void onClick(View v) {
 				Intent newIntent = new Intent(MainActivity.this, ManagerActivity.class);
 				MainActivity.this.startActivity(newIntent);
 			}
-		});
+		};
+		
+		final ImageButton manageButton = (ImageButton) findViewById(R.id.manageButton);
+		final TextView manageLabel = (TextView) findViewById(R.id.manageText);
+        
+        manageButton.setOnClickListener(manageButtonListener);
+        manageLabel.setOnClickListener(manageButtonListener);
     }
 
     @Override
