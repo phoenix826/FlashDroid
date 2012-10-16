b096453682297c5b64a060c78e491f72377f5ac2
mattaddy <mxa5942@rit.edu>
2012-10-16 19:01:04 -0400

    Edit card activity

--- src/com/usability/flashdroid/ManageCardsActivity.java
+++ src/com/usability/flashdroid/ManageCardsActivity.java
@@ -3,17 +3,24 @@ package com.usability.flashdroid;
 import com.usability.flashdroid.data_source.CardDataSource;
 import com.usability.flashdroid.data_source.DeckDataSource;
 import com.usability.flashdroid.db.DatabaseHelper;
+import com.usability.flashdroid.model.Card;
 import com.usability.flashdroid.model.Deck;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.support.v4.app.NavUtils;
 import android.support.v4.widget.SimpleCursorAdapter;
 
@@ -43,6 +50,7 @@ public class ManageCardsActivity extends Activity {
         deckSource.close();
         
         setListViewAdapter();
+        registerForContextMenu(cardList);
         
         TextView textView = (TextView) findViewById(R.id.cardsHeader);
         textView.setText(this.currentDeck.getName());
@@ -52,6 +60,42 @@ public class ManageCardsActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_manage_cards, menu);
         return true;
+    }
+    
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenuInfo menuInfo) {
+    	if (view.getId() == R.id.cardList) {
+    		menu.add(Menu.NONE, 1, 1, "Edit");
+    		menu.add(Menu.NONE, 2, 2, "Delete");
+    	}
+    }
+    
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+    	int position = item.getItemId();
+    	AdapterContextMenuInfo itemInfo = (AdapterContextMenuInfo) item.getMenuInfo();
+    	
+    	cardSource.open();
+    	final Card selectedCard = cardSource.getCardById(itemInfo.id);
+    	cardSource.close();
+    	
+    	switch (position) {
+    		case 1:
+    	    	Intent newIntent = new Intent(this, EditCardActivity.class);
+    	    	newIntent.putExtra("cardId", selectedCard.getId());
+    	    	newIntent.putExtra("deckId", currentDeck.getId());
+    	    	startActivity(newIntent);
+    			
+            	break;
+            	
+    		case 2:
+    			
+    	    	
+    	    	break;
+    	}
+
+    	
+    	return true;
     }
 
     
